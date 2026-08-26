c7f02892ad22d6675cc676139dc8f31119e43a72
Andy Zhou <130047947+ZhihaoZhou0@users.noreply.github.com>
2026-08-26 17:14:40 -0400

    Comments for Code
    Added functionality to prompt user for their name and display a welcome message.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
+# Return a welcome message using the provided name.
 def welcome_message(name):
     return f"{name}, welcome to the Data Engineering course."
 
 
 if __name__ == "__main__":
+    # Ask the user for their name and display the welcome message.
     name = input("Enter your name: ")
     print(welcome_message(name))
